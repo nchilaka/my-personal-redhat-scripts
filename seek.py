9e5d0115009f729fe3822530b3f757455351be1c
Nag Pavan Chilakam <nchilaka@redhat.com>
2019-10-05 00:29:03 +0530

    Update seek.py
    also adding script which can create multiple such files with a sleep also induced.

--- seek.py
+++ seek.py
@@ -8,3 +8,22 @@ for i in range(1000):
 src_file.close()
 dst_file.close()
 
+"""
+#e = random.randrange(1001, 100001, 2)
+# developed by nchilaka
+# this script writes to a file dst_file by seeking data from src_file at offset 0 and size of 1025 (ie to unalign with ec)
+import random, time
+
+for i in range(1, 1000000):
+        dest_file = 'dest_file' + str(i)
+        src_file = open("file", "r")
+        dst_file = open(dest_file, "a+")
+        for i in range(1000):
+                e = random.randrange(1001, 100001, 2)
+                src_file.seek(0, 0)
+                dst_file.write(src_file.read(e))
+        time.sleep(10)
+        src_file.close()
+        dst_file.close()
+
+"""
