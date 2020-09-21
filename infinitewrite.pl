fabe61cdd4c8b44ce74897aa5b5a144b628245b5
Nag Pavan Chilakam <nchilaka@redhat.com>
2020-09-21 21:56:32 +0530

    Update infinitewrite.pl
    adding notes for future reference

--- infinitewrite.pl
+++ infinitewrite.pl
@@ -1,3 +1,13 @@
+# Ref:  1625961 - Writes taking very long time leading to system hogging 
+# Ref: 1591208 - Writes taking very long time leading to system hogging 
+# open a file, keep writing to the file with overlapping writes infinitely
+# While the above was going on I did a reboot(or killed a brick) of n1(ie client-c0)
+# after that I saw different abnormalities such as below:
+# 1)doing ls -l from another mount was taking very long time even for displaying 10 files
+# 2) gluster v heal info was not responding even after 5min(only one file needs to be healed)
+# 3) fuse client process (glusterfs) from where the perl script was run had consumed 5.5GB memory
+# 4)file size on the backend were all different on each of the 1x3 bricks
+
 #!/usr/bin/perl
 $offset=0;
 #$len=2;
